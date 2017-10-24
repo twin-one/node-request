056fdbc85b50615094eae60d21871ed2370c0b11
Charley Ballantyne <charley@twin-one.com>
2017-10-24 08:41:42 -0700

    Lab finished

--- node-request.js
+++ node-request.js
@@ -1,5 +1,5 @@
 const request = require('request');
-request('http://www.google.com', function (error, response, body) {
+request('http://www.caddetails.com', function (error, response, body) {
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   console.log('body:', body); // Print the HTML for the Google homepage.
